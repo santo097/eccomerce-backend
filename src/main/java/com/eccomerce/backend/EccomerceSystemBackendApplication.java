fe2fdd1f44d0541a001f9e5f42c71c1fc15a8efe
Santiago Ramirez Moreno <santo.ebk@gmail.com>
2023-05-18 13:55:44 -0500

    Estructura de proyecto

---
+++ src/main/java/com/eccomerce/backend/EccomerceSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eccomerce.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccomerceSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccomerceSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eccomerce/backend/EccomerceSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eccomerce.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccomerceSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
