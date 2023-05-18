bd6a0efdf0dd9b7f15330d403f1b62e596682a98
Santiago Ramirez Moreno <santo.ebk@gmail.com>
2023-05-18 14:42:57 -0500

    Preparando el proyecto para su despliegue

--- src/main/java/com/eccomerce/backend/EccomerceSystemBackendApplication.java
+++ src/main/java/com/eccomerce/backend/EccomerceSystemBackendApplication.java
@@ -2,10 +2,12 @@ package com.eccomerce.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class EccomerceSystemBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EccomerceSystemBackendApplication.class, args);
 	}
